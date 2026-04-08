e9371974ac3ab6d36b559c930257b238db282a71
chickenchickenlove <ojt90902@naver.com>
2026-04-08 21:44:44 +0900

    t

--- core/src/test/scala/integration/kafka/network/DynamicConnectionQuotaTest.scala
+++ core/src/test/scala/integration/kafka/network/DynamicConnectionQuotaTest.scala
@@ -103,7 +103,7 @@ class DynamicConnectionQuotaTest extends BaseRequestTest {
     val maxConnectionsPerIPOverride = 7
     props.put(SocketServerConfigs.MAX_CONNECTIONS_PER_IP_OVERRIDES_CONFIG, s"localhost:$maxConnectionsPerIPOverride")
     reconfigureServers(props, perBrokerConfig = false, (SocketServerConfigs.MAX_CONNECTIONS_PER_IP_OVERRIDES_CONFIG, s"localhost:$maxConnectionsPerIPOverride"))
-    waitForMaxConnectionsOverrideApplied("127.0.0.0", maxConnectionsPerIPOverride)
+    waitForMaxConnectionsOverrideApplied("127.0.0.1", maxConnectionsPerIPOverride)
 
     verifyMaxConnections(maxConnectionsPerIPOverride, connectAndVerify)
   }
